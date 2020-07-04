2a087020eee8371658f01ca267a2a1c664cdfdd5
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2020-07-04 10:35:45 -0400

    Add title and subtitle fonts.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,20 +1,9 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/assets/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <div>
+      <h1 class="title">Never sleep again</h1>
+      <h2 class="subtitle">With our custom-made digital ghost replicas</h2>
+    </div>
   </Layout>
 </template>
 
@@ -26,8 +15,6 @@ export default {
 }
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
+<style lang="scss">
+@use '~/theme.scss';
 </style>
