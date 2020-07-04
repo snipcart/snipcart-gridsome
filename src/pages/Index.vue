baffea8eb39f7b53c75f461a01322d64cb827517
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2020-07-04 11:28:30 -0400

    Add bubble to Index page.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <div>
+    <div class="bubble">
       <h1 class="title">Never sleep again</h1>
       <h2 class="subtitle">With our custom-made digital ghost replicas</h2>
     </div>
@@ -17,4 +17,18 @@ export default {
 
 <style lang="scss">
 @use '~/theme.scss';
+
+.bubble {
+  border: 1px solid #c9d4fa;
+  border-radius: 14px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  height: 494px;
+  margin: 0 auto;
+  width: 93%;
+}
+
 </style>
