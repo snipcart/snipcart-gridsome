40523fe8fc72ce1228dd4724568a125050ae5380
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2020-07-06 14:07:52 -0400

    Add products collection.

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,9 +5,77 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const mockData = [
+  {
+    id: 0,
+    name: "Blinky",
+    images: {
+      big: "~/assets/ghosts/big/blinky.png",
+      small: "~/assets/ghosts/small/blinky.png"
+    },
+    description: {
+      short: "Pack leader",
+      long: "Natural leader. Gets a speed boost when pack pellets are cleared."
+    }
+  },
+  {
+    id: 1,
+    name: "Inky",
+    images: {
+      big: "~/assets/ghosts/big/inky.png",
+      small: "~/assets/ghosts/small/inky.png"
+    },
+    description: {
+      short: "Hungover but swift",
+      long: "Had a big night yesterday, but brace yourself, he is still pretty fast.",
+    }
+  },
+  {
+    id: 2,
+    name: "Pinky",
+    images: {
+      big: "~/assets/ghosts/big/pinky.png",
+      small: "~/assets/ghosts/small/pinky.png"
+    },
+    description: {
+      short: "Natural ambusher",
+      long: "Natural ambusher. Will cut you off. Be careful when you turn!"
+    }
+  },
+  {
+    id: 3,
+    name: "Clyde",
+    images: {
+      big: "~/assets/ghosts/big/clyde.png",
+      small: "~/assets/ghosts/small/clyde.png"
+    },
+    description: {
+      short: "Got chaotic moves",
+      long: "moves = Math.random() * Math.floor(10000000)",
+    }
+  }
+]
+
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
-    // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+  api.loadSource( async actions => {
+    const collection = actions.addCollection('Product')
+
+    const data = mockData
+
+    data.forEach(element => {
+      collection.addNode({
+        id: element.id,
+        name: element.name,
+        images: {
+          big: element.images.big,
+          small: element.images.small
+        },
+        description: {
+          short: element.description.short,
+          long: element.description.long
+        }
+      })
+    })
   })
 
   api.createPages(({ createPage }) => {
