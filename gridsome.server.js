54693c077dc9f30f2372b3a1b943d923c0ed4ce1
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2020-07-07 22:30:12 -0400

    Remove pages api demo call from gridsome.server.js.

--- gridsome.server.js
+++ gridsome.server.js
@@ -31,8 +31,4 @@ module.exports = function(api) {
       });
     });
   });
-
-  api.createPages(({ createPage }) => {
-    // Use the Pages API here: https://gridsome.org/docs/pages-api/
-  });
 };
