33ef991e645ec2727fb36e22ad63c1c4ef6a8b37
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2020-07-08 18:11:35 -0400

    Add image to product page.

---
+++ src/templates/Product.vue
@@ -0,0 +1,31 @@
+<template>
+  <g-image style="width: 100% height: 100$" :src="getImageUrl(this.$page.product.images.big, '@/assets/ghosts/big')" />
+</template>
+<page-query>
+query ($id: ID!) {
+  product(id: $id) {
+    name
+    images {
+        big
+    }
+    description {
+        long
+    }
+    price
+  }
+}
+</page-query>
+<script>
+export default {
+  methods: {
+    getImageUrl(url) {
+      // circumvents webpack issue with calling require in html
+      const imageFolderContext = require.context(
+        "@/assets/ghosts/big",
+        false
+      );
+      return imageFolderContext("./" + url);
+    }
+  }
+}
+</script>
