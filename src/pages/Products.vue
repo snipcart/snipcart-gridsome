8bded78f9eeea723e87565b821bd44ecb21896bf
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2020-07-10 10:00:42 -0400

    Detach paragraph layout from paragraph theme.

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,28 +1,31 @@
 <template>
-<Layout>
-  <div class="product-listing-wrapper">
-    <h1 class="title">Our ghosts</h1>
-    <p class="paragraph products-layout">Like your everlasting anxieties, our ghosts are there to keep you company through the good times and bad times alike.</p>
-    <Cards class="cards-wrapper"/>
-  </div>
-</Layout>
+  <Layout>
+    <div class="product-listing-wrapper">
+      <h1 class="title">Our ghosts</h1>
+      <p
+        class="paragraph products-layout"
+      >Like your everlasting anxieties, our ghosts are there to keep you company through the good times and bad times alike.</p>
+      <Cards class="cards-wrapper" />
+    </div>
+  </Layout>
 </template>
 <style lang="scss"scoped>
 @import "~/_theme.scss";
 
 .cards-wrapper {
-    width: 67%;
+  width: 67%;
 }
 
 .products-layout.paragraph {
+  margin: 29px auto 42px;
   width: 60%;
 }
 
 .product-listing-wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 }
 </style>
 <script>
